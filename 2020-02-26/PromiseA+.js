7610e5c0cacf7eb5eafef88217324ae23c311bc2
nanxg <1015687206@qq.com>
2020-03-07 22:37:42 +0800

    20200307

--- 2020-02-26/PromiseA+.js
+++ 2020-02-26/PromiseA+.js
@@ -1,7 +1,7 @@
 class Promise {
-    constructor(excutor){
+    constructor(excutor) {
         this.status = 'pedding'; // Promise初始状态
-        this.value = undefined; // 传给then的返回值
+        this.value = undefined; // resolve/reject传给then的值
         this.resolveAry = []; // 成功方法池
         this.rejectAry = []; //失败方法池
 
@@ -10,7 +10,7 @@ class Promise {
                 clearTimeout(timer);
                 if (this.status !== 'pedding') return;
                 this.status = 'fulfilled'; // 改变状态
-                this.value = res; // 改变返回值
+                this.value = res; // 改变传入的值
                 this.resolveAry.forEach(item => item(this.value))
             });
         };
@@ -21,19 +21,67 @@ class Promise {
                 this.status = 'rejected';
                 this.value = rej;
                 this.rejectAry.forEach(item => item(this.value))
-            });  
+            });
         };
-        excutor(resolveFn,rejectFn);
-    };
-    then(resolveCB,rejectCB){
-        this.resolveAry.push(resolveCB);
-        this.rejectAry.push(rejectCB);
-    };
-    catch(){
+
+        try {
+            excutor(resolveFn, rejectFn);
+        } catch (err) {
+            rejectFn(err)
+        }
 
     };
-    finally(){
+    then(resolveCB, rejectCB) {
+        // 简单版
+        // this.resolveAry.push(resolveCB);
+        // this.rejectAry.push(rejectCB);
 
+        // 链式调用版
+        // typeof resolveCB !== 'function' ? resolveCB = res => res : null;
+        // typeof rejectCB !== 'function' ? rejectCB = rej => {throw Error(rej)} : null; // 错误方式不一致
+        return new Promise((resolve, reject) => {
+            this.resolveAry.push(() => {
+                try {
+                    typeof resolveCB !== 'function' ? resolveCB = res => res : null;
+                    let x = resolveCB(this.value);
+                    x instanceof Promise ? x.then(resolve, reject) : resolve(x);
+                } catch (err) {
+                    reject(err);
+                }
+            });
+            this.rejectAry.push(() => {
+                try {
+                    if (typeof rejectCB === 'function') {
+                        let x = rejectCB(this.value);
+                        x instanceof Promise ? x.then(resolve, reject) : resolve(x);
+                    } else {
+                        reject(this.value)
+                    }
+                } catch (err) {
+                    reject(err);
+                }
+            });
+        })
+    };
+    catch(rejectCB) {
+        return this.then(null, rejectCB);
+    };
+    finally() {
+
+    };
+    static all(promiseAry = []) {
+        return new Promise((resolve, reject) => {
+            let index = 0, resolt = [];
+            for (let i = 0; i < promiseAry.length; i++) {
+                promiseAry[i].then(val => {
+                    index++;
+                    resolt[i] = val; // 索引和promiseAry的对应，保证结果顺序与数组相同(不能用push)
+                    index === promiseAry.length ? resolve(resolt) : null
+                }, val => {
+                    reject(val)
+                })
+            }
+        })
     }
 }
 
